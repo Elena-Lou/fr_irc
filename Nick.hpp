927ff7e0b1f00d74bb88140a89ab579fb337ef1e
Adrian WEAVER <aweaver@student.42.fr>
2023-06-01 11:37:51 +0200

    UPDATE NICK COMMAND CLASS:
    Adding Nick class.
    Needs to be tested thoroughly now.

--- Nick.hpp
+++ Nick.hpp
@@ -1,20 +1,13 @@
-
 #ifndef NICK_HPP
 # define NICK_HPP
 
 # include <list>
 # include <string>
-# include "ACommand.hpp"
 
-# define ERR_NEEDMOREPARAMS 461
-# define ERR_BANNEDFROMCHAN 474
-# define ERR_INVITEONLYCHAN 473
-# define ERR_BADCHANNELKEY 475
-# define ERR_CHANNELISFULL 471
-# define ERR_BADCHANMASK 476
-# define ERR_NOSUCHCHANNEL 403
-# define ERR_TOOMANYCHANNELS 405
-# define RPL_TOPIC 332
+# define ERR_NONICKNAMEGIVEN 431
+# define ERR_ERRONEUSNICKNAME 432
+# define ERR_NICKNAMEINUSE 433
+# define ERR_NICKCOLLISION 436
 
 class Client;
 class Server;
@@ -29,7 +22,7 @@ class Nick : public ACommand
 		Nick &operator=(const Nick &rhs);
 
 		void	execute() const;
-		void	error(std::string) const;
+		void	error(int errorCode) const;
 
 	private:
 		Nick();
