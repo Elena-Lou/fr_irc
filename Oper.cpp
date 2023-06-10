f0922f5802e2e6b386141784745585a87eda55ff
Elena-Lou <elouisiade@live.fr>
2023-06-10 23:55:30 +0200

    ADD ERROR CHECKS:
    Author's registration, nb of params and password are checked.
    
    switch cases are set and so is RPL if command is succesfull.
    
    The last check, the client is not authorised to become Oper depends on
    MODE command and will be completed later

--- Oper.cpp
+++ Oper.cpp
@@ -40,15 +40,59 @@ Oper & Oper::operator=(Oper const & rhs)
 
 void Oper::execute()
 {
-	std::cout << "execute()" << std::endl;
+	/* is the author registered ?*/
+	if (!this->_author->isRegistered())
+		return ;
+	
+	/* are there three params : command, user, pwd ? */
+	if (this->_cmd.size() != 3)
+	{
+		error(ERR_NEEDMOREPARAMS);
+		return ;
+	}
+
+	/* is the provided password correct ? */
+	if (this->_cmd[2] != this->_server->getPassword())
+	{
+		error(ERR_PASSWDMISMATCH);
+		return ;
+	}
+
+	/* is there something preventing the author from becoming an operator ? */
+	// to be determined when MODE is coded
+	this->confirm();
 }
 
 void Oper::error( int errorCode ) const
 {
-	std::cout << "error()" << std::endl;
+	switch (errorCode)
+	{
+	case ERR_NEEDMOREPARAMS:
+	{
+		this->_author->writeRPLToClient(this->_server,
+			ERR_NEEDMOREPARAMS, this->_cmd[0], MSG_NEEDMOREPARAMS);
+		break ;
+	}
+	case ERR_PASSWDMISMATCH:
+	{
+		this->_author->writeRPLToClient(this->_server,
+			ERR_PASSWDMISMATCH, MSG_PASSWDMISMATCH);
+		break ;
+	}
+	case ERR_NOOPERHOST:
+	{
+		this->_author->writeRPLToClient(this->_server,
+			ERR_NOOPERHOST, MSG_NOOPERHOST);
+		break;
+	}
+	default:
+		std::cerr << "Error: Unrecognised error code." << std::endl;
+		break;
+	}
 }
 
 void Oper::confirm() const
 {
-
+	this->_author->writeRPLToClient(this->_server,
+		RPL_YOUROPER, MSG_YOUROPER);
 }
